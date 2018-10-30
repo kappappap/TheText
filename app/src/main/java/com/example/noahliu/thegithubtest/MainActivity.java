56a2c0f21f1e1adf98a8a91cb78ce49f05b0d29f
NoahLiu <thumbb13555@gmail.com>
2018-10-30 22:22:55 +0800

    更改一些地方

--- app/src/main/java/com/example/noahliu/thegithubtest/MainActivity.java
+++ app/src/main/java/com/example/noahliu/thegithubtest/MainActivity.java
@@ -5,25 +5,40 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
         Button bt;
+        TextView Sam;
+        int Size = 50;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt=(Button) findViewById(R.id.button);
+        Sam = (TextView) findViewById(R.id.TeWeiLiu);
 
+        btClick();
+
+    }//onCreate
+
+    public void onclickBt (View view){
+//      Sam.setText(R.string.Theapp);
+        Sam.setText("這不是APP");
+        Sam.setTextSize(Size);
+
+    }//onclickBt
+
+    public void btClick(){
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("test","上傳測試");
-                Toast.makeText(getApplicationContext(),"Push",Toast.LENGTH_SHORT);//
+                Log.v("BT","1");
             }
         });
+    }//btClick
 
-    }
 
 
 }
